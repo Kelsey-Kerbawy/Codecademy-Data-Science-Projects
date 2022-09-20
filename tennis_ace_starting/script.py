a34abb39821c7c54fdfe388f9d510b3dfef8e8cf
KKerbawy <kelsey@wagyl.io>
2022-09-20 16:19:38 -0400

    Tennis Aces Multifactor Regression Project complete

--- tennis_ace_starting/script.py
+++ tennis_ace_starting/script.py
@@ -1,12 +1,13 @@
-import codecademylib3_seaborn
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 # load and investigate the data here:
-
-
+df = pd.read_csv("tennis_stats.csv")
+print(df.head())
+print(df.info())
+print(df.describe())
 
 
 
@@ -15,14 +16,14 @@ from sklearn.linear_model import LinearRegression
 
 # perform exploratory analysis here:
 
-
-
-
-
-
-
-
-
+plt.scatter(df.BreakPointsOpportunities,df.Winnings)
+plt.title('Break Points opportunities vs winnings')
+plt.show()
+plt.clf()
+plt.scatter(df.TotalPointsWon,df.Winnings)
+plt.title("Total points won vs winnings")
+plt.show()
+plt.clf()
 
 
 
